9bb834f39b39cb7ff4d40470d1ab342015895332
Jeffrey <jeffrey@pop-os.localdomain>
2023-01-05 11:50:19 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+#![warn(clippy::all,clippy::pedantic)]
+use bracket_lib::prelude::*;
+use serde::{Serialize,Deserialize};
+use serde_json::Result;
+use std::fs::{self};
+
+use std::path::Path;
+use std::ffi::OsStr;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ src/recipe.rs
@@ -0,0 +1,8 @@
+pub struct Recipe {
+    flour:i32,
+    hydration:i32,
+    starter:i32,
+    salt:f32,
+    yeast:f32,
+    oil:f32,
+}
